2d7adb1385f444927fd979bc4e095457c90936de
Ksundel <ak.samonova@yandex.ru>
2025-06-17 09:53:45 +0300

    less 3 end

---
+++ main.js
@@ -0,0 +1,43 @@
+//Домашка
+let number = prompt("Введите число")
+let diva = document.querySelector(".div_class")
+
+if (number >= 100){
+diva.innerHTML = number
+} else if (number < 100){
+    console.log(`Ваше число в консоли ${number}`)
+} else {
+    diva.innerHTML = "not number"
+    console.log ("Not number")
+}
+
+
+// document.getElementById("one").innerHTML = "eheh"
+
+
+//вывод содержимого инпута в консоль, по нажатию кнопки
+// let btn = document.querySelector(".check-in")
+// let inpt = document.querySelector(".text")
+
+// btn.onclick = function (){
+//     console.log("DA")
+//     console.log(inpt.value)
+// }
+
+
+// сообщение с полем ввода
+// let hisAge = prompt("Сколько тебе лет?")
+
+// if (hisAge == 22) {
+//     console.log("Yes")
+// } else if (hisAge < 18){
+//     console.log("you are so small))")
+// } else {
+//     console.log("No")
+// }
+
+
+//Вопрос
+// let div = document.getElementById("one");
+// div.innerHTML = "eheh"
+// console.log(diva.value)
